8fa07562332b9ffd6109b2b216d55800966a7ff3
Muharrem Şener Yüce <msyuce@hotmail.com>
2025-08-07 01:53:33 +0300

    01 - Login sayfasi kuruldu ve stiller hazirladi.

---
+++ config/company.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'name' => env('COMPANY_NAME', 'Varsayılan Firma'),
+    'logo' => env('COMPANY_LOGO', 'assets/images/logo.png'),
+    'background_color' => env('COMPANY_BG_COLOR', '#1c1c2e'),
+    'background_image' => env('COMPANY_BG_IMAGE', null),
+];
